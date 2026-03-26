c2cd99f51e6429d8dc4464b50d5a35f3f41bda67
DOUGLAS MICAEL NASCIMENTO DE SOUSA <douglas.mnsousa@senacsp.edu.br>
2026-03-25 21:26:15 -0300

    atualização 1

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const video = document.getElementById('bgVideo');
     video.playbackRate = 0.6;
+
+
+
+function ampliarImagem(botao) {
+    // Pega o caminho da imagem que está dentro do mesmo card do botão
+    const srcImagem = botao.parentElement.querySelector('img').src;
+    const overlay = document.getElementById('overlay');
+    const imgAmpliada = document.getElementById('img-ampliada');
+
+    imgAmpliada.src = srcImagem;
+    overlay.style.display = 'flex';
+}
+
+function fecharAmpliacao() {
+    document.getElementById('overlay').style.display = 'none';
+}
+
+// Fechar ao apertar a tecla "Esc"
+document.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") fecharAmpliacao();
+});
